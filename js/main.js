595a796405e96ddda15e88411fc9d2ff0f141be5
Edwin Trigos <82897868+Ewin24@users.noreply.github.com>
2023-08-29 07:27:10 -0500

    commit con el desarrollo de funcionalidad videos
    falta tomar variable del path y pasarla a la pagina videos para cargar el iframe

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-import { homePage, loadThumbnails } from "./getAll.js";
+import { homePage, loadThumbnails, loadVideo } from "./getAll.js";
+
+window.addEventListener('load', () => {
+    console.log(window.location);
+    window.location
+});
+
 
 
 homePage();
+loadVideo();
 loadThumbnails();
