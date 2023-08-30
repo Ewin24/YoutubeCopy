60c03eb5c7cc82ab92f10425ef3edb189cb517bd
ewin24 <edwintrigos24@gmail.com>
2023-08-29 20:31:19 -0500

    feat(js): barra de busqueda terminada feat(style): la pagina de videos contiene barra superior y aside

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
-import { homePage,loadVideo } from "./getAll.js";
+import { homePage, loadVideo, wrapSuggestions } from "./getAll.js";
 
 window.addEventListener('load', () => {
-    loadVideo(window.location.search.slice(7, window.location.search.slice.length -5));
-    console.log(window.location.search.slice(5));
+    loadVideo(window.location.search.slice(7, window.location.search.slice.length - 5));
 });
 
 
-
+let barraBusqueda = document.querySelector('#input-box');
+barraBusqueda.addEventListener('keyup', () => {
+    let elementoPadre = document.querySelector('#result-box')
+    if (barraBusqueda.value === "" || barraBusqueda.value === " ") {
+        document.querySelector('.result-box').style.display = 'none';
+    }else{
+        wrapSuggestions(barraBusqueda.value);
+    }
+}
+)
 homePage();
-// loadThumbnails();
