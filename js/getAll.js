b59292970b1413483f61f2857bdef5e45ced11cd
Ewin24 <edwintrigos24@gmail.com>
2023-08-30 05:55:36 +0200

    chore: Trabajo terminado
    - funcionando las busquedas directamente del canal del profe
    - videos del menu principal canal del profe
    - sidebar del video trae videos del canal del profe tambien

--- js/getAll.js
+++ js/getAll.js
@@ -13,7 +13,7 @@ let fetchApi = async () => {
     try {
         const response = await fetch(url, options);
         const result = await response.json();
-        console.error('carga de datos exitosa');
+        console.log('carga de datos exitosa');
         return result;
     } catch (error) {
         console.error('carga de datos fallida: ' + error);
@@ -24,11 +24,30 @@ let fetchApi = async () => {
 }
 
 
+let fetchSearch = async (q) => {
+    const url = `https://youtube138.p.rapidapi.com/channel/search/?id=UC8fkwsjcI_MhralEX1g4OBw&q=${q}&hl=en&gl=US`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '65ad5ad44bmsh773a97a88fa6da5p1534e5jsn00dec9fa147b',
+            'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 let homePage = async () => {
-    //let response = await fetchApi(); //garantiza que la variable se tome luego de que la promesa finalice
+    // let dataJson = await fetchApi(); //garantiza que la variable se tome luego de que la promesa finalice
     let data = await fetch('../data/channelVideos.json');
     let dataJson = await data.json();
-    let elementoPadre = document.querySelector('#row');
+    let elementoPadre = document.querySelector('#video-container');
     let videos = `
     ${dataJson.contents.map((value) => `
         <a class="col-12 col-lg-4 col-md-6 col-sm-12" href="../video.html?id='${value.video.videoId}'">
@@ -70,9 +89,7 @@ let homePage = async () => {
     `).join(" ")}
     `;
     elementoPadre.insertAdjacentHTML("beforeend", videos);
-
 };
-
 
 // let loadThumbnails = () => {
 //     const videoCards = [...document.querySelectorAll('.card')];
@@ -86,63 +103,39 @@ let homePage = async () => {
 //     });
 // }
 
-
 let loadVideo = async (id) => {
     //let response = await fetchApi(); //garantiza que la variable se tome luego de que la promesa finalice
     let elementoPadre = document.querySelector('#video');
     let videos = `
-
-    <iframe width="1280" height="720" 
+    <iframe width="720" height="480" 
         src="https://www.youtube.com/embed/${id}?si=1HNZAdjym_e6hwO1&autoplay=1" 
         title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
         clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
     </iframe>
-
     `;
     elementoPadre.insertAdjacentHTML("beforeend", videos);
 };
 
-
-let wrapSuggestions = async (query) => {
-    // const url = `https://youtube138.p.rapidapi.com/channel/search/?id=${channelId}&q=${query}&hl=en&gl=US`;
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '65ad5ad44bmsh773a97a88fa6da5p1534e5jsn00dec9fa147b',
-            'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
-        }
-    };
-
-    try {
-        const response = await fetch('../data/channelVideos.json');
-        const result = await response.json();
-        let elementoPadre = document.querySelector('#result-box');
-        let elementoSugerencias = `
-        ${result.contents.map((value) =>
-            `
-           <a href="../video.html?id='${value.video.vizdeoId}'">
+let wrapSuggestions = async (q) => {
+    let dataJson = await fetchSearch(q);
+    let elementoPadre = document.querySelector('#result-box');
+    let elementoSugerencias = `
+        ${dataJson.contents.map((value) =>
+        `
+           <a href="../video.html?id='${value.video.videoId}'">
                <li>
                    ${value.video.title}
                </li>
            </a>
        `).join(" ")} 
        `;
-        elementoPadre.innerHTML = '';
-        document.querySelector('.result-box').style.display = 'block';
-        elementoPadre.insertAdjacentHTML('beforeend', elementoSugerencias);
-    } catch (error) {
-        console.error(error);
-    }
+    elementoPadre.innerHTML = '';
+    document.querySelector('.result-box').style.display = 'block';
+    elementoPadre.insertAdjacentHTML('beforeend', elementoSugerencias);
 }
-
-//TODO: hacer el menu de busquedas 
-//TODO: hacer que el menu de suscripciones sea funcional
-//TODO: hacer que al dar clic al video redireccione a una nueva pagina con video
-
 
 export {
     homePage,
     wrapSuggestions,
     loadVideo
-    // loadThumbnails
 };
